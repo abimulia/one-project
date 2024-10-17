0536233cbd2500fef39b41e0afc5a383cb8e0d2c
abimulia <abimulia@gmail.com>
2024-10-17 23:02:10 +0700

    add new routing to game and highscores

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,22 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { GameComponent } from './game/game.component';
+import { HighscoresComponent } from './highscores/highscores.component';
 
 export const routes: Routes = [
     {
         path: 'home',
         title: 'Home Page',
         component: HomeComponent,
+    },
+    {
+        path: 'game',
+        title: 'Play Game',
+        component: GameComponent,
+    },
+    {
+        path: 'highscores',
+        title: 'High Scores',
+        component: HighscoresComponent,
     }
 ];
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, RouterOutlet,RouterLinkActive],
   templateUrl: './home.component.html',
   styleUrls: ['../app.component.css','./home.component.css']
 })
