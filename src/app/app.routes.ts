9546773a6a6017eb273d5703cbf1bb83e9986131
abimulia <abimulia@gmail.com>
2024-10-22 10:29:24 +0700

    add end component route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,21 +2,27 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { GameComponent } from './game/game.component';
 import { HighscoresComponent } from './highscores/highscores.component';
+import { EndComponent } from './end/end.component';
 
 export const routes: Routes = [
-    {
-        path: 'home',
-        title: 'Home Page',
-        component: HomeComponent,
-    },
-    {
-        path: 'game',
-        title: 'Play Game',
-        component: GameComponent,
-    },
-    {
-        path: 'highscores',
-        title: 'High Scores',
-        component: HighscoresComponent,
-    }
+  {
+    path: 'home',
+    title: 'Home Page',
+    component: HomeComponent,
+  },
+  {
+    path: 'game',
+    title: 'Play Game',
+    component: GameComponent,
+  },
+  {
+    path: 'highscores',
+    title: 'High Scores',
+    component: HighscoresComponent,
+  },
+  {
+    path: 'end',
+    title: 'Game End',
+    component: EndComponent,
+  },
 ];
