013e3bd37fb7ecfc2c763611e671dac259cba2fb
abimulia <abimulia@gmail.com>
2024-10-22 10:42:40 +0700

    add get highscores on ngOnInit()

--- src/app/highscores/highscores.component.ts
+++ src/app/highscores/highscores.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { Highscore } from '../highscore';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-highscores',
   standalone: true,
-  imports: [RouterLink,RouterOutlet, RouterLinkActive],
+  imports: [CommonModule,RouterLink,RouterOutlet, RouterLinkActive],
   templateUrl: './highscores.component.html',
   styleUrls: ['../app.component.css','./highscores.component.css']
 })
-export class HighscoresComponent {
+export class HighscoresComponent implements OnInit{
+  highScores: Highscore[] = [];
+
+  constructor(){}
+
+  ngOnInit(): void {
+    const storedHighScores = localStorage.getItem('highscores');
+    if(storedHighScores){
+      this.highScores = JSON.parse(storedHighScores);
+    }
+  }
 
 }
